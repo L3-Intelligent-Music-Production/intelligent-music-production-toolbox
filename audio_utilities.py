00b5b11ef286bde3a600fa9c5f54ec1b8d1ce44e
flavio <flavio.everardo@tec.mx>
2024-06-19 12:24:45 -0600

    Update audio_utilities.py

--- audio_utilities.py
+++ audio_utilities.py
@@ -1,5 +1,8 @@
 ## Audio utilities to handle music production code
 from math import log10
+from scipy.signal import resample
+import numpy as np
+import soundfile as sf
 
 def linear_amplitude_to_dBFS(amplitude):
     return 20 * log10(amplitude)
@@ -36,3 +39,18 @@ def normalize(tracks):
         tracks[tid] = normalized_audio
 
     return tracks
+
+def detect_audio_properties(audio_file):
+    ob = sf.SoundFile(audio_file)
+    num_channels = ob.channels
+    #print("%s   SR:%s   BD: %s   NumChannels: %s"%(audio_file, ob.samplerate, ob.subtype, ob.channels))
+    subtype = ob.subtype
+    if subtype == "FLOAT":
+        bit_depth = 32
+    elif subtype == "PCM_24":
+        bit_depth = 24
+    elif subtype == "PCM_16":
+        bit_depth = 16
+    else:
+        raise ValueError("Unsupported bit depth")
+    return bit_depth, num_channels
